322496fd9c17d9d1fd6d0a871736359b17321936
Jose Atel <pixelx55@gmail.com>
2024-06-24 16:12:13 -0300

    ultima actualizacion

--- js/wpp.js
+++ js/wpp.js
@@ -1,76 +1,82 @@
-    function enviarWhatsapp() {
-        var nombre = document.getElementById('nombre');
-        var email = document.getElementById('email');
-        var telefono = document.getElementById('telefono');
-        var mensaje = document.getElementById('mensaje');
+function enviarWhatsApp() {
+    var nombre = document.getElementById('nombre');
+    var email = document.getElementById('email');
+    var telefono = document.getElementById('telefono');
+    var mensaje = document.getElementById('mensaje');
 
-        var errorNombre = document.getElementById('error-nombre');
-        var errorEmail = document.getElementById('error-email');
-        var errorTelefono = document.getElementById('error-telefono');
+    var errorNombre = document.getElementById('error-nombre');
+    var errorEmail = document.getElementById('error-email');
+    var errorTelefono = document.getElementById('error-telefono');
 
-        // Reiniciar mensajes de error
-        errorNombre.textContent = '';
-        errorEmail.textContent = '';
-        errorTelefono.textContent = '';
+    // Reiniciar mensajes de error
+    errorNombre.textContent = '';
+    errorEmail.textContent = '';
+    errorTelefono.textContent = '';
 
-        // Validación de campos
-        if (nombre.value.trim() === '') {
-            nombre.style.border = '1px solid red';
-            nombre.focus();
-            return;
-        } else {
-            nombre.style.border = '1px solid #ccc';
-        }
-
-        if (email.value.trim() === '') {
-            email.style.border = '1px solid red';
-            email.focus();
-            return;
-        } else {
-            email.style.border = '1px solid #ccc';
-        }
-
-        // Validación de formato de correo electrónico
-        var emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
-        if (!emailRegex.test(email.value.toLowerCase())) {
-            email.style.border = '1px solid red';
-            email.focus();
-            errorEmail.textContent = 'Debe ser un correo válido';
-            errorEmail.style.color = 'red';
-            return;
-        } else {
-            email.style.border = '1px solid #ccc';
-        }
-
-        if (telefono.value.trim() === '') {
-            telefono.style.border = '1px solid red';
-            telefono.focus();
-            return;
-        } else {
-            telefono.style.border = '1px solid #ccc';
-        }
-
-        // Validación de formato de teléfono (numérico y 10 dígitos)
-        var telefonoRegex = /^[0-9]{10}$/;
-        if (!telefonoRegex.test(telefono.value)) {
-            telefono.style.border = '1px solid red';
-            telefono.focus();
-            errorTelefono.textContent = 'Debe ser un número válido de 10 dígitos';
-            errorTelefono.style.color = 'red';
-            return;
-        } else {
-            telefono.style.border = '1px solid #ccc';
-        }
-
-        // Construir el mensaje para WhatsApp
-        var textoMensaje = `Hola, mi nombre es ${nombre.value}, mi número de teléfono es ${telefono.value} y mi dirección de correo electrónico es ${email.value}. ${mensaje.value}`;
-
-        // URL base de WhatsApp
-        var baseUrl = 'https://wa.me';
-
-        // Componer la URL completa con el mensaje prellenado
-        var url = `${baseUrl}/+59894054380/?text=${encodeURIComponent(textoMensaje)}`;
-
-        // Abrir enlace en una nueva ventana
-        window.open(url, '_blank');
+    // Validación de campos
+    if (nombre.value.trim() === '') {
+        errorNombre.textContent = 'Campo obligatorio';
+        nombre.style.border = '1px solid red';
+        nombre.focus();
+        return;
+    } else if (!/^[A-Za-zñÑáéíóúÁÉÍÓÚ\s]+$/.test(nombre.value)) {
+        errorNombre.textContent = 'Solo se permiten letras y espacios';
+        nombre.style.border = '1px solid red';
+        nombre.focus();
+        return;
+    } else {
+        nombre.style.border = '1px solid #ccc';
     }
+
+    if (email.value.trim() === '') {
+        errorEmail.textContent = 'Campo obligatorio';
+        email.style.border = '1px solid red';
+        email.focus();
+        return;
+    } else {
+        email.style.border = '1px solid #ccc';
+    }
+
+    // Validación de formato de correo electrónico
+    var emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+    if (!emailRegex.test(email.value.toLowerCase())) {
+        errorEmail.textContent = 'Debe ser un correo válido';
+        email.style.border = '1px solid red';
+        email.focus();
+        return;
+    } else {
+        email.style.border = '1px solid #ccc';
+    }
+
+    if (telefono.value.trim() === '') {
+        errorTelefono.textContent = 'Campo obligatorio';
+        telefono.style.border = '1px solid red';
+        telefono.focus();
+        return;
+    } else {
+        telefono.style.border = '1px solid #ccc';
+    }
+
+    // Validación de formato de teléfono (numérico y 10 dígitos)
+    var telefonoRegex = /^[0-9]{10}$/;
+    if (!telefonoRegex.test(telefono.value)) {
+        errorTelefono.textContent = 'Debe ser un número válido de 10 dígitos';
+        telefono.style.border = '1px solid red';
+        telefono.focus();
+        return;
+    } else {
+        telefono.style.border = '1px solid #ccc';
+    }
+
+    // Construir el mensaje para WhatsApp
+    var textoMensaje = `Hola, mi nombre es ${nombre.value}, mi número de teléfono es ${telefono.value} y mi dirección de correo electrónico es ${email.value}. ${mensaje.value}`;
+
+    // URL base de WhatsApp
+    var baseUrl = 'https://wa.me';
+
+    // Componer la URL completa con el mensaje prellenado
+    var url = `${baseUrl}/NUMERO_DE_TELEFONO/?text=${encodeURIComponent(textoMensaje)}`;
+
+    // Abrir enlace en una nueva ventana
+    window.open(url, '_blank');
+}

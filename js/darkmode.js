23857f52cda943a3b4825fccc67af6a97d3733e3
Jose Atel <pixelx55@gmail.com>
2024-06-25 20:10:40 -0300

    ultima actualizacion

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,20 +1,34 @@
-const darkModeToggle = document.getElementById('darkModeToggle'); // Assuming you have a button with this ID
+  const darkModeToggle = document.getElementById('darkModeToggle');
+  const html = document.querySelector('html');
 
-darkModeToggle.addEventListener('click', () => {
-  const html = document.querySelector('html'); 
+  // Función para actualizar el texto del botón
+  const updateButtonText = () => {
+    if (html.classList.contains('dark')) {
+      darkModeToggle.textContent = 'Modo Claro';
+    } else {
+      darkModeToggle.textContent = 'Modo Oscuro';
+    }
+  };
 
-  // Toggle the 'dark' class on the root element
-  if (html.classList.contains('dark')) {
-    html.classList.remove('dark');
-    localStorage.setItem('darkMode', 'false'); // Store preference in local storage
-  } else {
+  darkModeToggle.addEventListener('click', () => {
+    // Toggle the 'dark' class on the root element
+    if (html.classList.contains('dark')) {
+      html.classList.remove('dark');
+      localStorage.setItem('darkMode', 'false'); // Store preference in local storage
+    } else {
+      html.classList.add('dark');
+      localStorage.setItem('darkMode', 'true'); // Store preference in local storage
+    }
+
+    // Actualizar el texto del botón
+    updateButtonText();
+  });
+
+  // Check if dark mode is enabled on page load
+  const darkModeEnabled = localStorage.getItem('darkMode') === 'true';
+  if (darkModeEnabled) {
     html.classList.add('dark');
-    localStorage.setItem('darkMode', 'true'); // Store preference in local storage
   }
-});
 
-// Check if dark mode is enabled on page load
-const darkModeEnabled = localStorage.getItem('darkMode') === 'true';
-if (darkModeEnabled) {
-  document.querySelector('html').classList.add('dark');
-}
+  // Actualizar el texto del botón al cargar la página
+  updateButtonText();

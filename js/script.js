6616949e2bb2da7c26101c0c390de12515b4bd62
Jose Atel <pixelx55@gmail.com>
2024-07-08 13:22:20 -0300

    Ultima actualizacion

--- js/script.js
+++ js/script.js
@@ -12,3 +12,19 @@ function loadComponent(id, url) {
         })
         .catch(error => console.error('Error loading component:', error));
 }
+
+// TASK
+document.addEventListener("DOMContentLoaded", () => {
+    loadComponent("task1", "tasks/task1.html");
+    loadComponent("task2", "tasks/task2.html");
+});
+
+function loadComponent(id, url) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(id).innerHTML = data;
+        })
+        .catch(error => console.error('Error loading component:', error));
+}
+

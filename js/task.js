7d3e1c28354245550993546ccb5839cfea692bef
Jose Atel <pixelx55@gmail.com>
2024-07-08 11:46:45 -0300

    Ultima actualizacion

--- js/task.js
+++ js/task.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     const taskContainer = document.getElementById('task-container');
-    const tasks = ['tasks/task1.md']; // Lista de archivos de tareas
+    const tasks = ['tasks/task1.md']; // Lista de archivos de tareas con la ruta correcta
 
     tasks.forEach(task => {
-        fetch(`tasks/${task}`)
+        fetch(task)
             .then(response => response.text())
             .then(data => {
-                const taskHTML = marked(data);
+                const taskHTML = marked(data); // Convierte Markdown a HTML usando marked.js
                 const taskDiv = document.createElement('div');
                 taskDiv.className = 'task';
                 taskDiv.innerHTML = taskHTML;

4d780d6a14acbe428585600e225df6284eafc4a7
Jose Atel <pixelx55@gmail.com>
2024-07-08 13:13:05 -0300

    Ultima actualizacion

--- js/task.js
+++ js/task.js
@@ -1,13 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
     const mainContainer = document.getElementById('main');
-    const tasks = ['tasks/task1.html']; // Lista de archivos HTML de tareas con la ruta correcta
+    const tasks = [
+        { file: 'tasks/task1.html', id: 'task1' },
+        { file: 'tasks/task2.html', id: 'task2' }
+        // Puedes añadir más archivos y sus IDs aquí según sea necesario
+    ];
 
+    // Itera sobre cada tarea y verifica si existe su contenedor correspondiente en el DOM
     tasks.forEach(task => {
-        fetch(task)
-            .then(response => response.text())
-            .then(data => {
-                mainContainer.innerHTML += data; // Inserta el contenido HTML directamente
-            })
-            .catch(error => console.error('Error al cargar la tarea:', error));
+        const { file, id } = task;
+        const taskContainer = mainContainer.querySelector(`#${id}`);
+
+        if (taskContainer) {
+            fetch(file)
+                .then(response => response.text())
+                .then(data => {
+                    taskContainer.innerHTML = data; // Inserta el contenido HTML directamente en el contenedor específico
+                })
+                .catch(error => console.error(`Error al cargar la tarea ${file}:`, error));
+        }
     });
 });

6bc7b0b963a66c59a6e194452e41a592b983b1cf
José Atel <46319904+Jose-Atel@users.noreply.github.com>
2024-09-10 20:00:28 -0300

    All website
    Reload full website

--- js/animacion.js
+++ js/animacion.js
@@ -1,25 +1,21 @@
+// animacion.js
 document.addEventListener('DOMContentLoaded', function() {
     const astronauta = document.querySelector('.astronauta');
     const body = document.body;
-    const overlay = document.querySelector('.overlay');
-    
+
     // Función para iniciar la animación del astronauta y el fondo
     function startAnimation() {
         astronauta.classList.add('moving');
         body.classList.add('zooming');
-        overlay.style.opacity = '1'; // Muestra la pantalla negra
 
         // Desactivar animaciones y ocultar la pantalla negra después del zoom
         setTimeout(function() {
             astronauta.classList.remove('moving');
             body.classList.remove('zooming');
-            overlay.style.opacity = '0'; // Oculta la pantalla negra
-        }, 6000); // Ajusta el tiempo según la duración de la animación
+        }, 15000); // Ajusta el tiempo según la duración de la animación
     }
 
-    // Listener para el clic en el enlace de retorno
-    document.querySelector('.home-link').addEventListener('click', function(e) {
-        e.preventDefault();
-        startAnimation();
-    });
+    // Listener para el clic en cualquier parte de la página
+    document.body.addEventListener('click', startAnimation);
+    document.body.addEventListener('touchstart', startAnimation);
 });
